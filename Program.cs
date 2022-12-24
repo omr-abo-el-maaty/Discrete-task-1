239dc442ecb7512873718b41c27e1049932b98b7
omr-abo-el-maaty <121355879+omr-abo-el-maaty@users.noreply.github.com>
2022-12-24 20:01:05 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace check_if_the_number_is_prime
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("write the first number : ");
+            int f = int.Parse(Console.ReadLine());
+            Console.Write("write the second number :");
+            int s = int.Parse(Console.ReadLine());
+            Console.WriteLine("the prime numbers : ");
+            for  (; f <= s; f++)
+            {
+                if (f < 2) continue;
+                bool x = true;
+                for (int n = 2 ; n <f; n++)
+                {
+                    if (f%n==0)
+                    {
+                       x= false;
+                        break;
+                    }
+                }
+                if (x==false)
+                {
+                    continue;
+                }
+               Console.WriteLine(f);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
